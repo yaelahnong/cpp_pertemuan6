557c8d680c3d69acd806bf72e08fda1f38217547
Marino Imola <45358825+yaelahnong@users.noreply.github.com>
2023-10-14 16:26:19 +0700

    Update program5.cpp

--- program5.cpp
+++ program5.cpp
@@ -13,18 +13,18 @@ int main()
 	};
 	
 	cout
-	<< setw(5) << "NIM"
-	<< setw(25) << "Nama"
-	<< setw(25) << "Alamat"
-	<< setw(25) << "HP"
-	<< endl;
+		<< setw(5) << "NIM"
+		<< setw(25) << "Nama"
+		<< setw(25) << "Alamat"
+		<< setw(25) << "HP"
+		<< endl;
 	
 	for (int i = 0; i < 3; i++) {
 		cout
-		<< setw(5) << Mahasiswa[i][0]
-		<< setw(25) << Mahasiswa[i][1]
-		<< setw(25) << Mahasiswa[i][2]
-		<< setw(25) << Mahasiswa[i][3]
-		<< endl;
+			<< setw(5) << Mahasiswa[i][0]
+			<< setw(25) << Mahasiswa[i][1]
+			<< setw(25) << Mahasiswa[i][2]
+			<< setw(25) << Mahasiswa[i][3]
+			<< endl;
 	}
 }
